863b41450345286c83571ac46b7d7aa722a6b6ab
yasotidev <yasotidev@outlook.fr>
2015-05-17 10:07:21 +0200

    Context commit.
    Add a default context data.

---
+++ 00/Src/Client/Web/BP00.Scaffolding/Data/DefaultContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using BP00.Scaffolding.Domain;
+
+namespace BP00.Scaffolding.Data
+{
+    public class DefaultContext : DbContext
+    {
+
+        public DefaultContext()
+            :base("name=BP-D")
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
